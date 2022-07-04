39cfdb16a5efefff951c35a26c5dcf0ccd402698
Park Irene <parkirene@Parkui-MacBookPro.local>
2022-07-04 18:38:33 +0900

    june 5th

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -13,8 +13,8 @@ export default function Featured() {
       <div className="bottom">
         <div className="featuredChart" style={{ width: 200, height: 200 }}>
           <CircularProgressbar
-            value={22.37}
-            text={"22.37%"}
+            value={24.44}
+            text={"24.44%"}
             strokeWidth={15}
             styles={buildStyles({
               pathColor: "rgb(14, 77, 161)",
@@ -24,7 +24,7 @@ export default function Featured() {
           />
         </div>
         <span className="objective">연 매출 24억 목표 대비</span>
-        <span className="period">2022년 6월 3주 기준</span>
+        <span className="period">2022년 6월 5주 기준</span>
       </div>
     </div>
   );
